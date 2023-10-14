b6676cfaf5d266d59ead6aca503814c54176e630
Shayaan Emran <shayaan.emran@gmail.com>
2023-10-14 12:03:51 -0400

    in

--- tests/testthat/test-neuralODE.R
+++ tests/testthat/test-neuralODE.R
@@ -30,3 +30,16 @@ test_that("Rk4 solver works", {
                  state = list(1.0, y))
   expect_equal(as.numeric(x[[2]][,1]), -0.125438, tolerance = 0.01)
 })
+
+test_that("Forward works", {
+  skip_if_no_tf()
+  ode_fun<- function(u){
+    r = u ^ 3
+    true_A = rbind(c(-0.1, 2.0), c(-2.0, -0.1))
+    du <- r %*% (true_A)
+    return(as.matrix(du))
+  }
+  y<- tf$cast(t(as.matrix(c(2, 0))), dtype = tf$float32)
+  x<- forward(ode_fun, inputs =  y,  tsteps = c(0, 0.25))
+  expect_equal(as.numeric(x[1,][1]), -0.125438, tolerance = 0.01)
+})
